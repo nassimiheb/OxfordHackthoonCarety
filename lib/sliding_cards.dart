d938232dfe980ea550ea111b60cb1e3fbac42e3d
Alaeddine-Nasri <ia_nasri@esi.dz>
2020-05-30 23:02:56 +0100

    Ranking page

--- lib/sliding_cards.dart
+++ lib/sliding_cards.dart
@@ -1,4 +1,4 @@
-import 'package:carety/congrat_page.dart';
+import 'congrat_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'dart:math' as math;
